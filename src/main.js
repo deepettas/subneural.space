c160925fe6bd59c0e7de563b8d003987ae97812b
noresources <dennis.petta@gmail.com>
2019-11-15 14:57:25 +0200

    hotjar install

--- src/main.js
+++ src/main.js
@@ -10,4 +10,6 @@ export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
   Vue.component('ResumeLayout', ResumeLayout);
+  head.script.push({src: '/hotjar.js'});
 }
+
